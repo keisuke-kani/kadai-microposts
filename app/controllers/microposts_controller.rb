04d2e0a3b270487fb4e1342a17e514182054b037
keisuke.kani <keisuke.kani@kiramex.com>
2019-04-15 10:07:08 +0000

    post

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,43 @@
+class MicropostsController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
+  def create
+    @micropost = microposts_resources.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @microposts = microposts_resources.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
+  end
+  
+
+
+  
+  private
+  
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
+  
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    unless @micropost
+      redirect_to root_url
+    end
+  end
+  
+  def microposts_resources # 2回目はDBに接続しないから早くなる。
+    @microposts_resources ||= current_user.microposts
+  end  
+  
+end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,18 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @micropost = microposts_resources.build
+      @microposts = microposts_resources.order('created_at DESC').page(params[:page])
+    end
   end
+  
+  
+  private
+  
+  
+  def microposts_resources # 2回目はDBに接続しないから早くなる。
+    @microposts_resources ||= current_user.microposts
+  end
+  
+  
 end
